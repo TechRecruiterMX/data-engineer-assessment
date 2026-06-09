d31c2739a60117f5c7dee063b7d98466c1be85d6
Amalga Group - Hiring Engineers <daniel.mtzwf@gmail.com>
2026-06-09 09:16:03 -0600

    Create challenge.py

---
+++ python/challenge.py
@@ -0,0 +1,19 @@
+sales = [
+    {"customer_id": 1, "amount": 100},
+    {"customer_id": 2, "amount": 200},
+    {"customer_id": 1, "amount": 150},
+    {"customer_id": 3, "amount": None},
+    {"customer_id": 2, "amount": 200},
+]
+
+"""
+Tasks
+
+1. Remove records with null amounts.
+
+2. Calculate total spend per customer.
+
+3. Return the highest spending customer.
+
+4. Explain how you would process this if there were 100 million rows.
+"""
